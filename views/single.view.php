b39f6bb8091cc989f4dbd4d4c1518813f379c9e1
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-04 17:54:33 +0000

    Navigation

--- views/single.view.php
+++ views/single.view.php
@@ -1,4 +1,4 @@
-<h2><?= $post['title']; ?></h2>
+<h1><?= $post['title']; ?></h1>
       <div class="body"><?= $post['body']; ?></div>      
 
       <p><button onclick="history.go(-1);">Back </button></p>
