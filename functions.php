ddeec57b6d8c25508bafc2e22eea40a892b8000e
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-05 20:38:24 +0000

    200 word posts

--- functions.php
+++ functions.php
@@ -18,3 +18,13 @@ function view($path, $data = null)
    include 'views/layout.php';
  
 }
+
+// shorting text on index blog posts
+function ellipsis($text, $max=100, $append='&hellip;')
+{
+    if (strlen($text) <= $max) return $text;
+    $out = substr($text,0,$max);
+    if (strpos($text,' ') === FALSE) return $out.$append;
+    return preg_replace('/\w+$/','',$out).$append;
+}
+
